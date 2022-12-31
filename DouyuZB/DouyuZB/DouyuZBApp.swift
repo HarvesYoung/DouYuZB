1b52dc4bae3c4c976306f25b9fe3b19ca05cb2e6
HarvesYoung <759897298@qq.com>
2023-01-01 01:24:21 +0900

    b

---
+++ DouyuZB/DouyuZB/DouyuZBApp.swift
@@ -0,0 +1,17 @@
+//
+//  DouyuZBApp.swift
+//  DouyuZB
+//
+//  Created by 杨宁波 on 2023/1/1.
+//
+
+import SwiftUI
+
+@main
+struct DouyuZBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
