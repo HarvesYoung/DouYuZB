b3edb2ac36bf36d9f383cd41001b81c0b729cf62
HarvesYoung <759897298@qq.com>
2023-01-01 23:08:59 +0900

    text模块练习

--- DouyuZB/DouyuZB/ContentView.swift
+++ DouyuZB/DouyuZB/ContentView.swift
@@ -1,24 +1,59 @@
-//  ContentView.swift
-//  DouyuZB
-//
-//  Created by 杨宁波 on 2023/1/1.
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+	var body:some View {
+		let price = 3.1444444
+		
+		VStack(alignment: .leading) {
+			Text("hello wolrd")
+				.foregroundColor(.blue) // 文字のカラーを定義する
+			.font(.body)
+			
+			Text(".body")
+				.font(.body)
+				.italic() //倾斜
+			
+			Text("subheadline")
+				.font(.subheadline)
+				.bold() // 加粗
+			
+			Text("title")
+				.font(.title)
+				.strikethrough(true, color: .red) // 中划线
+			
+			Text("headline")
+				.font(.headline)
+				.underline(true, color: .green)
+			
+			Text(".callout")
+				.font(.callout)
+			
+			Text("to be, or not to be, that is a question")
+				.frame(width: 100)
+				.lineLimit(2)
+			
+			HStack {
+				Text("$\(price, specifier: "%.2f")")
+				Spacer()
+				Text("hello harves")
+					.border(Color .blue)
+				Spacer()
+				Text("hello xiaolan")
+					.baselineOffset(2)
+			}
+			
+			Text("Date() = \(Date())")
+				.lineLimit(3)
+//				.padding()
+		} // VStack{} end
+		.padding()
+//		.foregroundColor(.blue)
+	}
+} // ContentView{} end
+
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
