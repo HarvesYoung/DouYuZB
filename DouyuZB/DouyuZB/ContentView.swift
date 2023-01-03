28e3fc7599a08df2d2b8036b68f7a94d5d7cb273
HarvesYoung <759897298@qq.com>
2023-01-04 01:04:20 +0900

    button控件

--- DouyuZB/DouyuZB/ContentView.swift
+++ DouyuZB/DouyuZB/ContentView.swift
@@ -2,12 +2,19 @@ import SwiftUI
 
 struct ContentView: View {
 	var body:some View {
+//		TextContainer
+//		AddContainer
+//		StyleButtonIOS
+		CustomButtonStyle
+	}
+	/*
+	var TextContainer:some View {
 		let price = 3.1444444
 		
 		VStack(alignment: .leading) {
 			Text("hello wolrd")
 				.foregroundColor(.blue) // 文字のカラーを定義する
-			.font(.body)
+				.font(.body)
 			
 			Text(".body")
 				.font(.body)
@@ -44,16 +51,111 @@ struct ContentView: View {
 			
 			Text("Date() = \(Date())")
 				.lineLimit(3)
-//				.padding()
+			//				.padding()
 		} // VStack{} end
 		.padding()
-//		.foregroundColor(.blue)
+		//		.foregroundColor(.blue)
+	} // TextContainer{} end
+	*/
+	
+	var AddContainer:some View {
+		List {
+			ForEach(items){ item in
+				Text("\(item.title) . \(item.desc)")
+			} // ForEach{} end
+			
+			Button("add item", action: addItem)
+		}
+	} // AddContainer{} end
+	@State private var items:[Item] = []
+	private func addItem(){
+		let newItem = Item(title: "a new title", desc: "a new desc")
+		items.append(newItem)
+	}
+	
+	var StyleButtonIOS: some View {
+		VStack(spacing: 20) {
+			Button("button1", action: signin)
+				.buttonStyle(PlainButtonStyle())
+			
+			Button("button2", action: signin)
+				.buttonStyle(BorderlessButtonStyle())
+			Button("button3", action: signin)
+				.buttonStyle(DefaultButtonStyle())
+		}
+	}
+	
+	
+	var CustomButtonStyle:some View {
+		// ButtonStyle 自定义样式使用系统默认的标准交互
+		// PrimitiveButtonStyle 自定义 样式的同时可以自定义交互逻辑
+		
+		VStack {
+			Button("Scale Effect", action:signin)
+				.buttonStyle(ScaleEffectButtonStyle())
+		}
 	}
 } // ContentView{} end
 
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+				ContentView()
+	}
+}
+
+var ButtonView:some View {
+	VStack {
+		Button(action: {
+			signin()
+		}, label: {
+			Text("signIn1")
+		})
+		
+		Button(action: signin) {
+			Text("signin2")
+		}
+		
+		Button("signin3"){
+		}
+		
+			HStack {
+				Button("register1") {
+					register()
+				}
+		
+				Button("register2", action:register)
+			}
+	} // VStack} end
+} // ButtonView{} end
+
+
+// 定义循环结构体
+struct Item:Identifiable {
+	let id = UUID()
+	let title: String
+	let desc: String
+}
+
+private func signin(){
+	print("signin按钮点击了..")
+	debugPrint("signin按钮被点击了..")
+}
+private func register(){}
+
+// primitive 定义button样式
+
+struct ScaleEffectButtonStyle:ButtonStyle {
+	func makeBody(configuration: Configuration) -> some View {
+		HStack {
+			Spacer()
+			configuration.label
+				.padding()
+				.foregroundColor(configuration.isPressed ? .red : .white)
+			Spacer()
+		}
+		.frame(width: 300)
+		.background(Color.blue.cornerRadius(10.0))
+		.scaleEffect(configuration.isPressed ? 0.9 : 1)
 	}
 }
